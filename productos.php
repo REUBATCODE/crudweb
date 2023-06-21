613513c93af5dc83fd44a4f148f9b87c4bb4ce5b
Ruben Ruiz <a22311151@uthermosillo.edu.mx>
2023-06-20 17:59:40 -0700

    CRUD de usuarios iniciado, INSERT de usuarios funcionando
    Signed-off-by: Ruben Ruiz <a22311151@uthermosillo.edu.mx>

--- productos.php
+++ productos.php
@@ -7,7 +7,7 @@
     <title>Abarrotes Ruben</title>
 </head>
 <body bgcolor="#7F9183">
-    <h1>Tres tristes tigres tragaban trigo en un trigal</h1>
+    Productos Usuarios <br>
     <?php
         /* PARA MOSTRAR ERRORES EN MAC */
         ini_set('display_errors', 1);       
@@ -16,36 +16,34 @@
 
         /* ESTABLECEMOS LA CONEXIÓN*/
         $conexion = new mysqli("localhost", "root", "", "pos") or die("Por alguna razón, no se pudo conectar al servidor.");
-        echo("Algo bien pal DJ<br>");  
         /* ESTABLECEMOS LAS VARIABLES DE COMANDO*/
         $queryselect = "SELECT * FROM productos";
         $comando= mysqli_query($conexion, $queryselect);
         /* UNA TABLA A FORMA DE GRID*/
         if($comando->num_rows>0)
         {
-            echo("Productos totales: ".$comando->num_rows);
+            echo "Productos totales: ".$comando->num_rows;
             echo "<table border='1' bgcolor='#B8B8AA'><tr><th colspan='5'>INVENTARIO DE ABARROTES RUBÉN</th><tr><th>Código</th><th>Producto</th></th><th>Precio</th><th>Eliminar</th><th>Modificar</th></tr>";
             while($registro=$comando->fetch_assoc())
             {
-                echo("\n\t<tr bgcolor='#B8B8AA'><td>".$registro["codigo"].
-                "</td>\n\t<td>".$registro["nombre"]."</td>\n\t<td>". $registro["precio"].
-                "</td> <td> <a href='delete.php?codigo=".$registro['codigo']
-                ."'><img src='./img/icon_delete.png'>
-                </a></td> <td><img src='./img/icon_update.png'></td> </tr>\n");
+                echo "\n\t<tr bgcolor='#B8B8AA'><td>".$registro["codigo"].
+                "</td>\n\t<td>".$registro["nombre"]."</td>\n\t<td>". $registro["precio"]."</td> 
+                <td> <a href='delete.php?codigo=".$registro['codigo']."'> <img src='./img/icon_delete.png'> </a> </td> 
+                <td> <a href='update.php?codigo=".$registro['codigo']."'> <img src='./img/icon_update.png'> </a> </td> </tr>\n";
             }
             echo "</table>";
         }
         else{
-            echo("No se encontraron registros en la DB.");
+            echo "No se encontraron registros en la DB.";
         }
   
     ?>
     <form action="insert.php" method="post">
         <fieldset style="width: 0px">
             <legend>Agregar nuevo producto</legend>
-                Codigo del producto: <input name="codigo" type="text" value='012'><br>
-                Nombre del producto: <input name="nombre" type="text" value='Caguama Miller'><br>
-                Precio del producto: <input name="precio" type="text" value='25.00'><br>
+                Codigo del producto: <input name="codigo" type="text" value=''><br>
+                Nombre del producto: <input name="nombre" type="text" value=''><br>
+                Precio del producto: <input name="precio" type="text" value=''><br>
             <input type="submit" value='Agregar producto'>       
         </fieldset>
     </form>
